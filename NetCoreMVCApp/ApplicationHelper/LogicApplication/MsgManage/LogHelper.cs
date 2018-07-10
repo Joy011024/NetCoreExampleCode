e609645c03c0536e443df011e3f743989de499fb
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-07-10 20:40:22 +0800

    2018-07-10 netcore 增加类库

---
+++ NetCoreMVCApp/ApplicationHelper/LogicApplication/MsgManage/LogHelper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LogicApplication
+{
+    public class LogHelper
+    {
+    }
+}
---
+++ NetCoreMVCApp/ApplicationHelper/LogicApplication/MsgManage/MsgHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicApplication
+{
+    class MsgHelper
+    {
+    }
+}
