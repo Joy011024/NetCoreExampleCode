f51d89942256a80252c4378b7f4b4bfbf9022d64
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-07-18 12:40:22 +0800

    2018-07-18  增加mvc.netcore ，对于配置文件从json读取进行帮助类封装【1.帮助类需要提供文件路径，否则查找路径在bin目录下】

--- NetCoreMVCApp/ApplicationHelper/LogicApplication/MsgManage/LogHelper.cs
+++ NetCoreMVCApp/ApplicationHelper/LogicApplication/MsgManage/LogHelper.cs
@@ -4,5 +4,19 @@ namespace LogicApplication
 {
     public class LogHelper
     {
+        void WriteLog()
+        {
+            FileLogHelper fl = new FileLogHelper();
+            //记录文件日志
+            DBLogHelper dbfl = new DBLogHelper();
+            //记录日志到数据库
+
+        }
     }
+    class FileLogHelper
+    {
+
+    }
+    class DBLogHelper
+    { }
 }
---
+++ NetCoreMVCApp/NetCoreMvcWebExample/Controllers/AppConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+namespace NetCoreMvcWebExample
+{
+    public class AppConfig
+    {
+        static IConfigurationRoot cr;
+        public static void Init()
+        {
+            if (cr == null)
+            {
+                cr = new JsonAppConfigHelper("appsettings.json").GetConfiguration();
+                /*
+             System.TypeInitializationException:“The type initializer for 'NetCoreMvcWebExample.AppConfig' threw an exception.”
+            The configuration file 'appsettings.json' was not found and is not optional. The physical path is 'x\bin\Debug\netcoreapp2.0\appsettings.json'.
+            json文件没复制到输出目录
+             */
+            }
+        }
+        static AppConfig()
+        {
+            Init();
+        }
+        static string mainConn;
+        public static string MainDBConn
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(mainConn))
+                {
+                   mainConn= cr.GetConnectionString("DBConnStrinbg");
+                }
+                return mainConn;
+            }
+        }
+    }
+    /// <summary>
+    /// 目前查找json文件时已知默认路径在bin目录下
+    /// </summary>
+    public class JsonAppConfigHelper
+    {
+        string jsonfile;
+        /// <summary>
+        /// 需要加载的json
+        /// </summary>
+        /// <param name="jsonFileName"></param>
+        public JsonAppConfigHelper(string jsonFileName)
+        {
+            jsonfile = jsonFileName;
+        }
+        public IConfigurationRoot GetConfiguration()
+        {
+            var build = new ConfigurationBuilder();
+            build.AddJsonFile(jsonfile);
+            return build.Build();
+        }
+
+    }
+}
